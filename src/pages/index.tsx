633297fc15f97f3cc12180be981fa45ed229056f
ZzzandyzzZ <andy_gean@hotmail.com>
2023-04-21 19:44:58 -0500

    Add mui components to test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,15 @@
+import { Box, Button, TextField, Typography } from '@mui/material'
+
 export default function Home(): JSX.Element {
-  return <h1>HOLA</h1>
+  return (
+    <>
+      <Box bgcolor="red" width={300} height={300}>
+        <Typography color="white" variant="h4">
+          HOLA A TODOS
+        </Typography>
+      </Box>
+      <Button> HOLA</Button>
+      <TextField label="INGRESA ALGO" />
+    </>
+  )
 }
