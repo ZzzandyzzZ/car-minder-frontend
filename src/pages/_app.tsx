7b274900e1634f226591bfce85b5e42cb0c8d41e
ZzzandyzzZ <andy_gean@hotmail.com>
2023-04-23 13:48:08 -0500

    Add provider to date field

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,29 @@
 import type { AppProps } from 'next/app'
 
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import { CacheProvider } from '@emotion/react'
+import { LocalizationProvider } from '@mui/x-date-pickers'
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
-import { CacheProvider } from '@emotion/react'
+
 import blueTheme from '@theme/blueTheme'
 import createEmotionCache from '@theme/createEmotionCache'
+
 import '@styles/globals.css'
-// import '@fontsource/roboto/300.css'
-// import '@fontsource/roboto/400.css';
-// import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css'
 
 // Client-side cache, shared for the whole session of the user in the browser.
-
 const clientSideEmotionCache = createEmotionCache()
 
 export default function CarMinderApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <CacheProvider value={clientSideEmotionCache}>
-      <ThemeProvider theme={blueTheme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <ThemeProvider theme={blueTheme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </LocalizationProvider>
     </CacheProvider>
   )
 }
