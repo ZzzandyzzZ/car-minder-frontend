2d09360cd1fe7646c66706e82734b18eb3afd2c2
ZzzandyzzZ <andy_gean@hotmail.com>
2023-05-18 19:49:15 -0500

    Create test form with formik

--- src/modules/record/components/RecordCreationForm.tsx
+++ src/modules/record/components/RecordCreationForm.tsx
@@ -1,35 +1,63 @@
-import { TextField } from '@mui/material'
-import { DatePicker } from '@mui/x-date-pickers'
 import { useRouter } from 'next/navigation'
+import { Box, Button, TextField } from '@mui/material'
+import { DatePicker } from '@mui/x-date-pickers'
+import { useFormik } from 'formik'
 
+const initialValues = {
+  date: '',
+  carId: '',
+  user: '',
+  service: '',
+}
 export function RecordCreationForm(): JSX.Element {
   const { push } = useRouter()
+  const formik = useFormik({
+    initialValues,
+    onSubmit: (values) => {
+      console.log(JSON.stringify(values, null, 2))
+    },
+  })
   return (
-    <>
+    <Box component="form" onSubmit={formik.handleSubmit}>
       <DatePicker label="Fecha" slotProps={{ textField: { fullWidth: true } }} />
       <TextField
+        id="carId"
         label="Placa del carro"
+        name="carId"
+        value={formik.values.carId}
+        onChange={formik.handleChange}
+        error={true}
+        helperText="requerido"
         inputProps={{ style: { textAlign: 'center' } }}
         fullWidth
-        disabled
-        onClick={() => {
-          push('/dashboard/car/create')
-        }}
+        // disabled
+        // onClick={() => {
+        //   push('/dashboard/car/create')
+        // }}
       />
       <TextField
+        id="user"
         label="Dueño del carro"
+        name="user"
+        value={formik.values.user}
+        onChange={formik.handleChange}
+        error={true}
+        helperText="requerido"
         inputProps={{ style: { textAlign: 'center' } }}
         fullWidth
-        InputLabelProps={{
-          shrink: false,
-        }}
         onClick={() => {
           push('/dashboard/user/create')
         }}
         disabled
       />
       <TextField
+        id="service"
         label="Trabajo realizado"
+        name="service"
+        value={formik.values.service}
+        onChange={formik.handleChange}
+        error={true}
+        helperText="requerido"
         inputProps={{ style: { textAlign: 'center' } }}
         fullWidth
         disabled
@@ -37,6 +65,7 @@ export function RecordCreationForm(): JSX.Element {
           push('/dashboard/service/create')
         }}
       />
-    </>
+      <Button type="submit">Registrar</Button>
+    </Box>
   )
 }
