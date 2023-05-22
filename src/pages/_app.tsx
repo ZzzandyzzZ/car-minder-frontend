7f4ab5885907e7763df03fcc2a039998187495da
ZzzandyzzZ <andy_gean@hotmail.com>
2023-05-21 19:23:57 -0500

    Update LocalizationProvider language to spanish

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,18 @@
-import type { AppProps } from 'next/app'
-
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
 import { CacheProvider } from '@emotion/react'
+import { es } from 'date-fns/locale'
 import { LocalizationProvider } from '@mui/x-date-pickers'
+import { Provider } from 'react-redux'
 import { ThemeProvider } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline'
+import { type AppProps } from 'next/app'
 
+import { store } from 'store/store'
 import blueTheme from '@theme/blueTheme'
 import createEmotionCache from '@theme/createEmotionCache'
 
+import CssBaseline from '@mui/material/CssBaseline'
 import '@fontsource/roboto/700.css'
 import 'global.css'
-import { Provider } from 'react-redux'
-import { store } from 'store/store'
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache()
@@ -20,7 +20,7 @@ const clientSideEmotionCache = createEmotionCache()
 export default function CarMinderApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <CacheProvider value={clientSideEmotionCache}>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={es}>
         <ThemeProvider theme={blueTheme}>
           <Provider store={store}>
             <CssBaseline />
